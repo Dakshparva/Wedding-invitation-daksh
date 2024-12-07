2123d8a6e3fcb1da63092422f905d7729c8cebb0
Dakshathkumar N <101206655+Dakshparva@users.noreply.github.com>
2024-12-07 19:44:43 +0530

    fixed audio issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,9 +22,9 @@ export default function Hero() {
     <source src={dakshVideo} type="video/mp4" />
     Your browser does not support the video tag.
   </video>
-  <audio autoplay loop hidden>
+  {/* <audio autoplay loop hidden>
 		<source src="../../audio/BadukinaBannave.mp3" type="audio/mp3"/>
-	</audio>
+	</audio> */}
 
   {/* Overlay for darkening the background */}
   <div className="absolute inset-0 bg-black bg-opacity-50"></div>
